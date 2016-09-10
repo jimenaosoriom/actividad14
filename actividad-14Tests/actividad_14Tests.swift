a9eb73df94306b68bd28dd4b0b3175690257e9d8
user121331 <user121331@ny085.local>
2016-09-10 18:04:51 -0400

    Initial Commit

---
+++ actividad-14Tests/actividad_14Tests.swift
@@ -0,0 +1,36 @@
+//
+//  actividad_14Tests.swift
+//  actividad-14Tests
+//
+//  Created by user121331 on 9/10/16.
+//  Copyright © 2016 user121331. All rights reserved.
+//
+
+import XCTest
+@testable import actividad_14
+
+class actividad_14Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
